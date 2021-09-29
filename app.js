44bbb9913153358510f22befad33f80bd3a27072
NOAHDELTOROSMYTH <NOAHSMYTH96@GMAIL.COM>
2021-09-29 15:52:20 -0700

    added sound files and created working JS to play sound file when image is clicked

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 // import functions and grab DOM elements
+const astleyImage1 = document.getElementById('Astley-image-1');
+const astleySound1 = document.getElementById('Astley-sound-1');
+const astleyImage2 = document.getElementById('Astley-image-2');
+const astleySound2 = document.getElementById('Astley-sound-2');
+const astleyImage3 = document.getElementById('Astley-image-3');
+const astleySound3 = document.getElementById('Astley-sound-3');
 
 // initialize global state
 
@@ -6,3 +12,14 @@
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
+astleyImage1.addEventListener('click', ()=>{
+  astleySound1.play();
+});
+
+astleyImage2.addEventListener('click', ()=>{
+  astleySound2.play();
+});
+
+astleyImage3.addEventListener('click', ()=>{
+  astleySound3.play();
+});
